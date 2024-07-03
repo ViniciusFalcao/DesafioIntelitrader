d77d167205b14a8d3ed924574d48387703f6d63d
Vinicius Canuto Falcão <viniciusfalcao82@gmail.com>
2024-07-03 14:32:01 -0300

    trocando as posições

---
+++ DesafioIntelitrader/DesafioInteliTrader.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioIntelitrader
+{
+    public class DesafioInteliTrader
+    {
+
+        public string Desafio1(StringBuilder str )
+        {   
+            var msg=str.ToString().Trim();
+            var txt = "abcdefghijklmnopq";
+            var charA = txt.ToCharArray();
+
+            var rep = txt.Length / 8;
+            Console.WriteLine("repetições: " + rep);
+            var index = 0;
+            for (int i = 0; i < rep; i++)
+            {
+                if (i == 0)
+                {
+                    index += 7;
+
+                    var a = charA[index];
+                    var b = charA[index + 1];
+                    charA[index] = b;
+                    charA[index + 1] = a;
+                    Console.WriteLine("mudou");
+                    Console.WriteLine(charA);
+                }
+                else
+                {
+
+                    index += 8;
+                    var a = charA[index];
+                    var b = charA[index + 1];
+                    charA[index] = b;
+                    charA[index + 1] = a;
+                    Console.WriteLine(charA);
+
+
+                }
+
+
+            }
+
+            //tranformar array de char em string
+            var txt2=txt;
+            rep = 0;
+            index = 0;
+
+            rep = txt.Length / 4;
+            for (int i = 0; i < rep; i++)
+            {
+                if (i==0)
+                {
+                    index += 4;
+
+                    Console.WriteLine(txt2.Substring(0,index));
+
+                }
+                else
+                {
+                    var antes = index;
+                    index += 4;
+                    var depois=index;
+
+                    var subs = txt2.Substring(antes,depois);
+                    Console.WriteLine(subs);
+
+
+
+                }
+            }
+
+
+
+            return "";
+        }
+
+    }
+}
--- DesafioIntelitrader/Program.cs
+++ DesafioIntelitrader/Program.cs
@@ -1,41 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DesafioIntelitrader;
 using System.Runtime.InteropServices.Marshalling;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
-Console.WriteLine("Hello, World!");
+
+
+
+
  var msg=new StringBuilder("10010110 11110111 01010110 00000001 " +
      "00010111 00100110 01010111 00000001 00010111 01110110 01010111" +
-     " 00110110 11110111 11010111 01010111 00000011").ToString().Trim();
-
-var txt = "abcdefghijklmnopq";
-var charA=txt.ToCharArray();
-
-var num=(txt.Length/4)+1;
-Console.WriteLine("repetições: "+num);
-var num2 = 0;
-for (int i = 0; i < num; i++)
-{
-   Console.WriteLine(txt[num2]);
-    if (i == 0)
-    {
-        num2 += 3;
-
-        var a = charA[num2 ];
-        var b =charA[num2 + 1];
-        charA[num2] = charA[num2 + 1];
-    }
-    else
-    {
-        num2 += 4;
-
-        var a = charA[num2];
-        var b = charA[num2 + 1];
-        charA[num2] = charA[num2 + 1];
-    }
-
-
-}
-
-
+     " 00110110 11110111 11010111 01010111 00000011");
+var desafio1 = new DesafioInteliTrader().Desafio1(msg);
 Console.ReadLine();
