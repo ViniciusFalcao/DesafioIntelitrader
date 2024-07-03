449f63cd2f8a5871b87c4a192f6296de5932b0bd
Vinicius Canuto Falcão <viniciusfalcao82@gmail.com>
2024-07-03 16:06:05 -0300

    fazendo ajustes
    me enganei com os sinais

--- DesafioIntelitrader/DesafioInteliTrader.cs
+++ DesafioIntelitrader/DesafioInteliTrader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace DesafioIntelitrader
@@ -12,7 +13,8 @@ namespace DesafioIntelitrader
         public string Desafio1(StringBuilder str )
         {   
             var msg=str.ToString().Trim();
-            var txt = "abcdefghijklmnopq";
+            var txt = "abcdefghijklmnop";
+            Console.WriteLine(txt.Length);
             var charA = txt.ToCharArray();
 
             var rep = txt.Length / 8;
@@ -25,57 +27,75 @@ namespace DesafioIntelitrader
                     index += 7;
 
                     var a = charA[index];
-                    var b = charA[index + 1];
+                    var b = charA[index - 1];
                     charA[index] = b;
-                    charA[index + 1] = a;
-                    Console.WriteLine("mudou");
-                    Console.WriteLine(charA);
+                    charA[index - 1] = a;
+                    //Console.WriteLine("mudou");
+                    //Console.WriteLine(charA);
                 }
                 else
                 {
 
                     index += 8;
                     var a = charA[index];
-                    var b = charA[index + 1];
+                    var b = charA[index - 1];
                     charA[index] = b;
-                    charA[index + 1] = a;
-                    Console.WriteLine(charA);
+                    charA[index - 1] = a;
+                    //Console.WriteLine(charA);
 
+
+
+                   
 
                 }
-
+                
 
             }
-
-            //tranformar array de char em string
-            var txt2=txt;
+            var txt2=new String(charA);
+            Console.WriteLine(txt2);
             rep = 0;
             index = 0;
 
-            rep = txt.Length / 4;
+            rep = txt2.Length / 4;
+
+            List<string> list = new List<string>();
             for (int i = 0; i < rep; i++)
             {
                 if (i==0)
                 {
                     index += 4;
+                    list.Add(txt2.Substring(0, index));
 
-                    Console.WriteLine(txt2.Substring(0,index));
+                   
 
                 }
                 else
                 {
                     var antes = index;
                     index += 4;
-                    var depois=index;
 
-                    var subs = txt2.Substring(antes,depois);
-                    Console.WriteLine(subs);
+                    var subs = txt2.Substring(antes,4);
+                    list.Add(subs);
+                   // Console.WriteLine(subs);
 
 
 
                 }
             }
+            var list2 = new List<string>();
+            index = 0;
+            for (int i = 0; i < list.Count/2; i++)
+            {
 
+                var prim=list.ElementAt(i);
+                var seg= list.ElementAt(i+1);
+                list2.Add(seg);
+                list2.Add(prim);
+                index += 2;
+
+            }
+            
+            //list.ForEach((i) =>Console.WriteLine(i));
 
 
             return "";
